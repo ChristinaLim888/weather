8353b378d34d1586bebeb20575431d2ad2667343
ChristinaLim888 <Christinalim666@gmail.com>
2025-09-01 23:25:23 +0700

    first commit main

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// const prompt = require('prompt-sync')({ sigint: true });
+
+//Module System
+import promptSync from 'prompt-sync';
+const prompt = promptSync({ sigint: true });
+
+// Konfigurasi dotenv
+import dotenv from 'dotenv';
+dotenv.config();
+
+let API_KEY = process.env.API_KEY;
+console.log(API_KEY);
+
+const UNITS = "metric";
